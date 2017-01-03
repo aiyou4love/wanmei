1da1e3e894c1a4463bed0e4b25b43b492949ddfa
aiyou4love <aiyou4love@outlook.com>
2017-01-03 16:06:38 +0800

    u

---
+++ cmake/windows/agent.bat
@@ -0,0 +1,10 @@
+@echo off
+::::::::::::::::::::::设置工程名称::::::::::::::::::::::
+set PROJNAME=agent
+set DRIVER=%~d0
+set PROJPATH=%cd%
+::::::::::::::::::::::调用编译脚本::::::::::::::::::::::
+call %SLN_ROOT%/resource/window/cmake/cmake.bat
+::::::::::::::::::::::::编译完成::::::::::::::::::::::::
+pause
+@echo on
---
+++ table/window/social.bat
@@ -0,0 +1,10 @@
+@echo off
+::::::::::::::::::::::设置工程名称::::::::::::::::::::::
+set PROJNAME=social
+set DRIVER=%~d0
+set PROJPATH=%cd%
+::::::::::::::::::::::调用表格脚本::::::::::::::::::::::
+call %SLN_ROOT%/resource/window/table/table.bat
+::::::::::::::::::::::::表格完成::::::::::::::::::::::::
+pause
+@echo on
